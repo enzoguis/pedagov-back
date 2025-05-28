d51ddc326f28a23c76179aafc3c3fbbbc238fff5
enzoguis <enzog0315@gmail.com>
2025-05-28 16:58:18 -0300

    feat: adding create occurrences controller and testing it

---
+++ src/infra/http/controllers/create-occurrence.controller.e2e-spec.ts
@@ -0,0 +1,92 @@
+import { AppModule } from '@/infra/app.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import { PedagogueFactory } from 'test/factories/make-pedagogue'
+import { AttachmentFactory } from 'test/factories/make-attachments'
+import { StudentFactory } from 'test/factories/make-student'
+import request from 'supertest'
+import { TeacherFactory } from 'test/factories/make-teacher'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { GroupFactory } from 'test/factories/make-group'
+
+describe('Create Occurrence (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let pedagogueFactory: PedagogueFactory
+  let attachmentFactory: AttachmentFactory
+  let studentFactory: StudentFactory
+  let teacherFactory: TeacherFactory
+  let groupFactory: GroupFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [
+        PedagogueFactory,
+        AttachmentFactory,
+        StudentFactory,
+        TeacherFactory,
+        GroupFactory,
+      ],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    pedagogueFactory = moduleRef.get(PedagogueFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
+    studentFactory = moduleRef.get(StudentFactory)
+    teacherFactory = moduleRef.get(TeacherFactory)
+    groupFactory = moduleRef.get(GroupFactory)
+
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /occurrences', async () => {
+    const author = await pedagogueFactory.makePrismaPedagogue()
+
+    const teacher = await teacherFactory.makePrismaTeacher()
+
+    const group = await groupFactory.makePrismaGroup({
+      teacherId: teacher.id,
+    })
+
+    const student = await studentFactory.makePrismaStudent({
+      groupId: group.id,
+    })
+
+    const attachment = await attachmentFactory.makePrismaAttachment()
+
+    const accessToken = await jwt.sign({ sub: author.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .post('/occurrences')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'Aluno faltou à aula',
+        description: 'O aluno não compareceu à aula de matemática.',
+        studentsIds: [student.id.toString()],
+        attendeesIds: [author.id.toString()],
+        attachmentsIds: [attachment.id.toString()],
+        teacherId: teacher.id.toString(),
+        type: 'ABSENCES',
+        shouldSendEmail: true,
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    const occurrenceOnDatabase = await prisma.occurrence.findFirst({
+      where: {
+        title: 'Aluno faltou à aula',
+      },
+    })
+
+    expect(occurrenceOnDatabase).toBeTruthy()
+  })
+})
---
+++ src/infra/http/controllers/create-occurrence.controller.ts
@@ -0,0 +1,75 @@
+import { CreateOccurrenceUseCase } from '@/domain/occurrences/application/use-cases/create-occurrence'
+import { OccurrenceTypeEnum } from '@/domain/occurrences/enterprise/entities/occurrence'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth-guard'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { UserPayload } from '@/infra/auth/jwt-strategy'
+import { EmailSenderTemplateIdEnum } from '@/domain/occurrences/application/email/email-sender'
+
+const createOccurrenceBodySchema = z.object({
+  title: z.string().min(1, 'The title must have at least 1 character'),
+  description: z.string(),
+  studentsIds: z.array(z.string().uuid()),
+  attendeesIds: z.array(z.string().uuid()),
+  attachmentsIds: z.array(z.string().uuid()),
+  teacherId: z.string().uuid(),
+  type: z.preprocess(
+    (val) => (typeof val === 'string' ? val.toUpperCase() : val),
+    z.nativeEnum(OccurrenceTypeEnum)
+  ),
+  shouldSendEmail: z.boolean(),
+})
+
+type CreateOccurrenceBody = z.infer<typeof createOccurrenceBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createOccurrenceBodySchema)
+
+@Controller('/occurrences')
+@UseGuards(JwtAuthGuard)
+export class CreateOccurrenceController {
+  constructor(private createOccurrence: CreateOccurrenceUseCase) {}
+
+  @Post()
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(bodyValidationPipe) body: CreateOccurrenceBody
+  ) {
+    const {
+      attachmentsIds,
+      attendeesIds,
+      description,
+      shouldSendEmail,
+      studentsIds,
+      teacherId,
+      title,
+      type,
+    } = body
+
+    const result = await this.createOccurrence.execute({
+      attachmentsIds,
+      attendeesIds,
+      authorId: user.sub,
+      description,
+      shouldSendEmail,
+      studentsIds,
+      teacherId,
+      title,
+      type,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    return { result: result.value }
+  }
+}
