be08ce4301b6a9d51a4fd798390c804472552b08
enzoguis <enzog0315@gmail.com>
2025-06-04 17:24:47 -0300

    feat: adding mailersender

--- src/infra/email/email.module.ts
+++ src/infra/email/email.module.ts
@@ -1,14 +1,33 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { NodeMailerSender } from './nodemailer-sender'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MailerModule } from '@nestjs-modules/mailer'
 import { EmailSender } from '@/core/email/email-sender'
+import { MailerSender } from './mailersender-sender'
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          service: 'gmail',
+          auth: {
+            user: config.get('GMAIL_AUTH_USER'),
+            pass: config.get('GMAIL_AUTH_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: `"Escola" <${config.get('GMAIL_AUTH_USER')}>`,
+        },
+      }),
+    }),
+  ],
   providers: [
     {
       provide: EmailSender,
-      useClass: NodeMailerSender,
+      useClass: MailerSender,
     },
   ],
   exports: [EmailSender],
