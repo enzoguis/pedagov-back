024966cc3205cc08a3649b11c2302354202e06d6
enzoguis <enzog0315@gmail.com>
2025-06-05 18:46:27 -0300

    feat: adding student name in email sender

--- src/infra/email/mailersender-sender.ts
+++ src/infra/email/mailersender-sender.ts
@@ -7,17 +7,15 @@ export class MailerSender implements EmailSender {
   constructor(private mailerService: MailerService) {}
 
   async send(message: EmailSenderSendParams): Promise<void> {
-
     try {
       await this.mailerService.sendMail({
         to: 'enzog0315@gmail.com',
         subject: 'Nova ocorrência registrada',
-        text: `Seu filho teve uma nova ocorrência escolar registrada`,
+        text: `Seu filho ${message.data.studentName} teve uma nova ocorrência escolar registrada`,
       })
       console.log('E-mail enviado com sucesso!')
     } catch (error) {
       console.error('Erro ao enviar e-mail:', error)
-      throw error
     }
   }
 }
